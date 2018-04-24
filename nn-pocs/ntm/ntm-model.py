ff3b86efd39a107234eeb48f8b8155849202891a
cristid9 <cristi.dnla@gmail.com>
2018-04-25 01:52:49 +0300

    Added skeleton for the ntm model

---
+++ nn-pocs/ntm/ntm-model.py
@@ -0,0 +1,8 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.optimizers import SGD
+import numpy as np
+
+if __name__ == "__main__":
+    pass
