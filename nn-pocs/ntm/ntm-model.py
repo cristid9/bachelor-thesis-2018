67839a6395ee7cf3db8d8fd1239d37f864d9481a
cristid9 <cristi.dnla@gmail.com>
2018-05-03 04:54:32 +0300

    Updated model's implementation

--- nn-pocs/ntm/ntm-model.py
+++ nn-pocs/ntm/ntm-model.py
@@ -1,8 +1,57 @@
-import keras
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation
-from keras.optimizers import SGD
+import theano
+import theano.tensor as T
 import numpy as np
+import matplotlib.pyplot as plt
 
-if __name__ == "__main__":
-    pass
+from lasagne.layers import InputLayer, DenseLayer, ReshapeLayer
+import lasagne.layers
+import lasagne.nonlinearities
+import lasagne.updates
+import lasagne.objectives
+import lasagne.init
+
+from ntm.layers import NTMLayer
+from ntm.memory import Memory
+from ntm.controllers import GRUController
+from ntm.heads import WriteHead, ReadHead
+from ntm.updates import graves_rmsprop
+
+from utils.generators import SortTask
+from utils.visualization import Dashboard
+
+
+def model(input_var, batch_size=1, size=8, num_units=100, memory_shape=(128, 20)):
+
+    # Input Layer
+    l_input = InputLayer((batch_size, None, size + 1), input_var=input_var)
+    _, seqlen, _ = l_input.input_var.shape
+
+    # Neural Turing Machine Layer
+    memory = Memory(memory_shape, name='memory', memory_init=lasagne.init.Constant(1e-6), learn_init=False)
+    controller = GRUController(l_input, memory_shape=memory_shape,
+        num_units=num_units, num_reads=1,
+        nonlinearity=lasagne.nonlinearities.rectify,
+        name='controller')
+    heads = [
+        WriteHead(controller, num_shifts=3, memory_shape=memory_shape, name='write', learn_init=False,
+            nonlinearity_key=lasagne.nonlinearities.rectify,
+            nonlinearity_add=lasagne.nonlinearities.rectify),
+        ReadHead(controller, num_shifts=3, memory_shape=memory_shape, name='read', learn_init=False,
+            nonlinearity_key=lasagne.nonlinearities.rectify)
+    ]
+    l_ntm = NTMLayer(l_input, memory=memory, controller=controller, heads=heads)
+
+    # Output Layer
+    l_output_reshape = ReshapeLayer(l_ntm, (-1, num_units))
+    l_output_dense = DenseLayer(l_output_reshape, num_units=size + 1, nonlinearity=lasagne.nonlinearities.sigmoid, \
+        name='dense')
+    l_output = ReshapeLayer(l_output_dense, (batch_size, seqlen, size + 1))
+
+    return l_output, l_ntm
+
+
+if __name__ == '__main__':
+    # Define the input and expected output variable
+    input_var, target_var = T.tensor3s('input', 'target')
+
+    
