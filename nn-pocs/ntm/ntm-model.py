c98bb514d265df0b5bd7582554e09fe65a282de0
cristid9 <cristi.dnla@gmail.com>
2018-05-24 17:55:14 +0300

    Finally added a working model

--- nn-pocs/ntm/ntm-model.py
+++ nn-pocs/ntm/ntm-model.py
@@ -1,7 +1,6 @@
 import theano
 import theano.tensor as T
 import numpy as np
-import matplotlib.pyplot as plt
 
 from lasagne.layers import InputLayer, DenseLayer, ReshapeLayer
 import lasagne.layers
@@ -20,13 +19,11 @@ from utils.generators import SortTask
 from utils.visualization import Dashboard
 
 
-def model(input_var, batch_size=1, size=8, num_units=100, memory_shape=(128, 20)):
+def model(input_var, batch_size=1, size=8, num_units=100, memory_shape=(1024, 160)):
 
-    # Input Layer
     l_input = InputLayer((batch_size, None, size + 1), input_var=input_var)
     _, seqlen, _ = l_input.input_var.shape
 
-    # Neural Turing Machine Layer
     memory = Memory(memory_shape, name='memory', memory_init=lasagne.init.Constant(1e-6), learn_init=False)
     controller = GRUController(l_input, memory_shape=memory_shape,
         num_units=num_units, num_reads=1,
@@ -41,7 +38,6 @@ def model(input_var, batch_size=1, size=8, num_units=100, memory_shape=(128, 20)
     ]
     l_ntm = NTMLayer(l_input, memory=memory, controller=controller, heads=heads)
 
-    # Output Layer
     l_output_reshape = ReshapeLayer(l_ntm, (-1, num_units))
     l_output_dense = DenseLayer(l_output_reshape, num_units=size + 1, nonlinearity=lasagne.nonlinearities.sigmoid, \
         name='dense')
@@ -51,7 +47,52 @@ def model(input_var, batch_size=1, size=8, num_units=100, memory_shape=(128, 20)
 
 
 if __name__ == '__main__':
-    # Define the input and expected output variable
-    input_var, target_var = T.tensor3s('input', 'target')
+    input_var = T.tensor3('input')
+    target_var = T.tensor3('target')
 
-    
+
+    generator = SortTask(batch_size=1, max_iter=25, size=8, max_length=5, end_marker=True)
+    l_output, l_ntm = model(input_var, batch_size=generator.batch_size, \
+    size=generator.size, num_units=100, memory_shape=(1024, 160))
+
+    pred_var = T.clip(lasagne.layers.get_output(l_output), 1e-6, 1. - 1e-6)
+    loss = T.mean(lasagne.objectives.binary_crossentropy(pred_var, target_var))
+
+    params = lasagne.layers.get_all_params(l_output, trainable=True)
+    updates = graves_rmsprop(loss, params, learning_rate=1e-2)
+
+    train_fn = theano.function([input_var, target_var], loss, updates=updates)
+    ntm_fn = theano.function([input_var], pred_var)
+    ntm_layer_fn = theano.function([input_var], lasagne.layers.get_output(l_ntm, get_details=True))
+
+    try:
+        scores, all_scores = [], []
+        for i, (example_input, example_output) in generator:
+            score = train_fn(example_input, example_output)
+            print("=" * 100)
+            print(i)
+            print(score)
+            print("=" * 100)
+            scores.append(score)
+            all_scores.append(score)
+            if i % 500 == 0:
+                mean_scores = np.mean(scores)
+                print ('Batch #%d: %.6f' % (i, mean_scores))
+                scores = []
+    except KeyboardInterrupt:
+        pass
+
+    print("Debug point")
+
+    markers = [
+            {
+            'location': (lambda params: params['length']),
+            'style': {'color': 'red'}
+        }
+    ]
+
+    dashboard = Dashboard(generator=generator, ntm_fn=ntm_fn, ntm_layer_fn=ntm_layer_fn, \
+        memory_shape=(128, 20), markers=markers, cmap='bone')
+
+    params = generator.sample_params()
+    dashboard.sample(**params)
