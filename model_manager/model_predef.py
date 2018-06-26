29d01eb1394c15497d3518c86abb45cd52c9f693
cristid9 <cristi.dnla@gmail.com>
2018-06-26 10:47:38 +0300

    Added file with model constants

---
+++ model_manager/model_predef.py
@@ -0,0 +1,14 @@
+IP = '127.0.0.1'
+PORT = 2018
+BUFFER_SIZE = 1024
+
+# models type
+MODEL_TYPES = [
+    "bubble_sorter",
+    "insertion_sorter"
+]
+
+TRAINED_MODELS_LOCATION = {
+    "bubble_sorter": "/tmp/models/bubble/ntm.zip"
+    # "bubble_sorter": "/tmp/models/bubble/ntm.zip"
+}
