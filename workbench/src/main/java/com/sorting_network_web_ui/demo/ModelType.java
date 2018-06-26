8c4e9b9e1493495c938f9f83d64b49bfaf5091ab
cristid9 <cristi.dnla@gmail.com>
2018-06-26 10:45:42 +0300

    Added implementation for model manager connector

---
+++ workbench/src/main/java/com/sorting_network_web_ui/demo/ModelType.java
@@ -0,0 +1,12 @@
+package com.sorting_network_web_ui.demo;
+
+public enum ModelType {
+    BUBBLE_SORTER("BUBBLE_SORTER"),
+    INSERTION_SORTER("INSERTION_SORTER");
+
+    private final String name;
+
+    ModelType(String s) {
+        name = s;
+    }
+}
---
+++ workbench/src/main/resources/static/handlers.js
@@ -0,0 +1,20 @@
+/**
+ * jQuery handlers for UI-controls present in the page
+ */
+
+/*
+$("#generate")  .on('click', function(e) {
+    console.log("Click on generate detected...");
+}); 
+
+$("#chose_another_model").on('click', function(e) {
+    console.log("Another model is going to be chosen...");
+}); 
+
+
+$("#reset_num_wires").on('click', function(e) {
+    console.log("Reset number of wires...");
+}); 
+*/
+
+
